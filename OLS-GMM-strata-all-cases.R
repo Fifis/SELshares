f502368647d06d0af971860675626eb763dac073
Fifis <fifis.himik@gmail.com>
2018-05-25 11:09:31 +0200

    Made output much nicer (CSV format); recomputed the results with the new design

--- OLS-GMM-strata-all-cases.R
+++ OLS-GMM-strata-all-cases.R
@@ -1,8 +1,6 @@
 rm(list=ls());
 source("functions.R")
-sink(file = "runlogs.txt", append = FALSE) # Flush the output file
 print(Sys.time())
-sink()
 
 library(parallel)
 library(sandwich)
@@ -27,17 +25,13 @@ sample.sizes <- c(50, 150, 500) # Original sample size BEFORE stratification
 methods <- c("OLS", "GMM")
 
 # Calculate true shares by approximation
-Nbig <- 1e7 # With this value, the block below should run in under 5 seconds
-set.seed(20170808)
-Xs <- rlnorm(Nbig, sd = sdXs)
-Us <- rnorm(Nbig, sd = sdUs)
-Xbeta <- cbind(1, Xs) %*% c(beta0, beta1)
-Ys <- Xbeta + Us # Homoskedastic data
-Q.hom <- sum(Ys<boundary)/Nbig
-Us <- Us*sqrt(.1+.2*Xs+.3*Xs^2)
-Ys <- Xbeta + Us # Heteroskedastic data
-Q.het <- sum(Ys<boundary)/Nbig
-rm(Xs, Xbeta, Us, Ys)
+Nbig <- 999999 # With this value, the block below should run in under 1 minute
+nrep <- 20
+Q.hom <- mclapply(1:nrep, function(sid) {Zhom <- generate.data(N=Nbig, params=params, boundary=boundary, Pl=c(1,1), strat.var="Y", heteroskedastic=FALSE, seed=sid); return(sum(Zhom$Y<boundary)/nrow(Zhom))}, mc.cores = num.workers)
+Q.het <- mclapply(1:nrep, function(sid) {Zhet <- generate.data(N=Nbig, params=params, boundary=boundary, Pl=c(1,1), strat.var="Y", heteroskedastic=TRUE,  seed=sid); return(sum(Zhet$Y<boundary)/nrow(Zhet))}, mc.cores = num.workers)
+Q.hom <- mean(unlist(Q.hom))
+Q.het <- mean(unlist(Q.het))
+Q.X <- plnorm(boundary)
 print("Estimated theoretical aggregate shares, starting simulations...")
 
 ##########################
@@ -45,7 +39,7 @@ print("Estimated theoretical aggregate shares, starting simulations...")
 ##########################
 
 # These loops will run through all possible model designs w.r.t. skedasticity, stratification variable,
-# OLS/GMM method and sample sizes, outputting the annotated statistics into runlogs.txt in the current working directory
+# OLS/GMM method and sample sizes, outputting the statistics into a table
 
 # Uncomment this to try specific designs
 # skedasis <- c(TRUE)
@@ -53,87 +47,69 @@ print("Estimated theoretical aggregate shares, starting simulations...")
 # sample.sizes <- c(50, 150, 500)
 # methods <- c("OLS", "GMM")
 
+results <- data.frame(array(NA, dim=c(length(skedasis)*length(strat.vars)*length(sample.sizes)*length(methods), 4+6+3+6)))
+names(results) <- c("Heteroskedastic", "Stratification", "N", "Method",
+                    "beta0.Bias", "beta0.SE", "beta0.RMSE", "beta1.Bias", "beta1.SE", "beta1.RMSE", "Q.Bias", "Q.SE", "Q.RMSE",
+                    "size1.HC0", "size1.HC3", "size2.HC0", "size2.HC3", "power1.HC0", "power1.HC3")
+thisrow <- 1
+
 for (heteroskedastic in skedasis) {
   for (strat.var in strat.vars) {
     for (N in sample.sizes) {
       for (method in methods) {
-        
         start.time <- Sys.time() 
-        stime	<- mclapply(1:MC, function(sid) StratSampOLS(N = N, params = params, boundary = boundary, Pl = Pl, strat.var = strat.var, method = method, heteroskedastic = heteroskedastic, powersize = powersize, seed = sid), mc.cores = num.workers)
-        coeff <- data.frame(do.call("rbind", (lapply(stime, '[[', "coefficients"))) ) # Extract only the estimated coefficients
-        shares <- do.call("c", (lapply(stime, '[[', "shares"))) # Extract only estimated shares
-        colnames(coeff)[1] <- "Intercept"
+        results$Heteroskedastic[thisrow] <- heteroskedastic
+        results$Stratification[thisrow] <- strat.var
+        results$N[thisrow] <- N
+        results$Method[thisrow] <- method
+        
+        res	<- mclapply(1:MC, function(sid) StratSampOLS(N = N, params = params, boundary = boundary, Pl = Pl, strat.var = strat.var, method = method, heteroskedastic = heteroskedastic, powersize = powersize, seed = sid), mc.cores = num.workers)
+        coeff <- data.frame(do.call("rbind", (lapply(res, '[[', "coefficients"))) ) # Extract only the estimated coefficients
+        shares <- do.call("c", (lapply(res, '[[', "shares"))) # Extract only estimated shares
+        colnames(coeff) <- c("beta0", "beta1")
+        
+        results[thisrow, c("beta0.Bias", "beta1.Bias")] <- colMeans(sweep(coeff, 2, params[1:2]))
+        results[thisrow, c("beta0.SE", "beta1.SE")] <- sapply(coeff, sd)
+        results[thisrow, c("beta0.RMSE", "beta1.RMSE")] <- sqrt(colMeans(sweep(coeff, 2, params[1:2])^2))
         
         # Computing share statistics for the GMM case
         if (method=="GMM") {
-          Q.true <- if (strat.var=="X") plnorm(boundary) else (if(heteroskedastic) Q.het else Q.hom)
-          share.bias <- mean(shares) - Q.true
-          share.msd <- sd(shares)
-          share.rmse <- sqrt(mean((Q.true - shares)^2))
-          share.table <- c(share.bias, share.msd, share.rmse)
-          names(share.table) <- c("Share bias", "Share SE", "Share RMSE")
+          Q.true <- if (strat.var=="X") Q.X else (if(heteroskedastic) Q.het else Q.hom)
+          results$Q.Bias[thisrow] <- mean(shares) - Q.true
+          results$Q.SE[thisrow] <- sd(shares)
+          results$Q.RMSE[thisrow] <- sqrt(mean((Q.true - shares)^2))
         }
         
-        # Computing coefficient statistics
-        bias <- colMeans(sweep(coeff, 2, params[1:2]))
-        msd <- sapply(coeff, sd)
-        rmse <- sqrt(colMeans(sweep(coeff, 2, params[1:2])^2))
-        
-        # The calculated RMSE might experience precision issue: see with the fragment below
-        # Uncomment these in order to check precision
-        # rmse2 = sqrt(bias^2 + msd^2)
-        # print((rmse2 - rmse)/rmse2) # Relative difference
-        
-        err.table <- t(rbind(bias, msd, rmse))
-        rownames(err.table) <- c("Intercept", "X")
-        colnames(err.table) <- c("Bias", "SE", "RMSE")
-        err.table2 <- matrix(c(err.table[1, ], err.table[2, ]), nrow=1)
-        colnames(err.table2) <- c("Intercept Bias", "Intercept SE", "Intercept RMSE", "Slope Bias", "Slope SE", "Slope RMSE")
-        
         if (powersize) {
-          zstatsHC0 <- data.frame(do.call("rbind", (lapply(stime, '[[', "zstatHC0"))) )
-          zstatsHC3 <- data.frame(do.call("rbind", (lapply(stime, '[[', "zstatHC3"))) )
-          Wald <- data.frame(do.call("rbind", (lapply(stime, '[[', "Wald"))) )
+          zstatsHC0 <- data.frame(do.call("rbind", (lapply(res, '[[', "zstatHC0"))) )
+          zstatsHC3 <- data.frame(do.call("rbind", (lapply(res, '[[', "zstatHC3"))) )
+          Wald <- data.frame(do.call("rbind", (lapply(res, '[[', "Wald"))) )
           colnames(zstatsHC0) <- c("t0", "t1")
           colnames(zstatsHC3) <- c("t0", "t1")
-          h0HC0 <- sum(abs(zstatsHC0$t0) >= qnorm(0.975))/MC
-          h1HC0 <- sum(abs(zstatsHC0$t1) >= qnorm(0.975))/MC
-          h0HC3 <- sum(abs(zstatsHC3$t0) >= qnorm(0.975))/MC
-          h1HC3 <- sum(abs(zstatsHC3$t1) >= qnorm(0.975))/MC
-          WaldHC0 <- sum(Wald$WaldHC0 >= qchisq(0.95, 2))/MC
-          WaldHC3 <- sum(Wald$WaldHC3 >= qchisq(0.95, 2))/MC
+          results[thisrow, c("size1.HC0", "size1.HC3", "size2.HC0", "size2.HC3", "power1.HC0", "power1.HC3")] <- 
+          c(sum(abs(zstatsHC0$t1) >= qnorm(0.975))/MC, sum(abs(zstatsHC3$t1) >= qnorm(0.975))/MC,
+            sum(Wald$WaldHC0 >= qchisq(0.95, 2))/MC, sum(Wald$WaldHC3 >= qchisq(0.95, 2))/MC,
+            sum(abs(zstatsHC0$t0) >= qnorm(0.975))/MC, sum(abs(zstatsHC3$t0) >= qnorm(0.975))/MC)
         }
         
         # Uncomment to obtain density plots
-        # den.const <- density(coeff$Intercept, bw="ucv")
-        # den.X <- density(coeff$X, bw="ucv")
-        # pdf(paste0("coef-", (if(method=="GMM") "gmm" else "ols"), (if (heterosk) "-het" else "-hom") , "-strat-", strat.var, "-", N, ".pdf"), 7, 5)
+        # den.const <- density(coeff$beta0, bw="ucv")
+        # den.X <- density(coeff$beta1, bw="ucv")
+        # pdf(paste0("coef-", (if(method=="GMM") "gmm" else "ols"), (if (heteroskedastic) "-het" else "-hom") , "-strat-", strat.var, "-", N, ".pdf"), 7, 5)
         # plot(den.X, lwd=2, xlim=range(den.X$x, den.const$x), ylim=range(0, den.X$y, den.const$y),
         # main=paste0("MC estimates with stratification by ", strat.var, ", N=", N), xlab=paste0("N = ", MC))
         # lines(den.const, lwd=2, lty=2)
         # legend("topright", c("Slope", "Intercept"), lwd=2, lty=c(1, 2))
         # dev.off()
         
-        msg <- paste0((if (heteroskedastic) "Heterosk." else "Homosk."), (if(method=="GMM") " GMM" else " OLS"), ", strat. by ", strat.var, ", N=", N, ", MC=", MC)
         elapsed <- difftime(Sys.time(), start.time, units="secs")
-        sink(file = "runlogs.txt", append = TRUE)
-        cat(msg, "\n")
-        print(round(elapsed, 2))
-        print(err.table2)
-        if (method=="GMM") print(share.table)
-        if (powersize) {
-          cat(paste0("Empirical test size (nominal size 5%): the true H0: beta1=", params[2], " was rejected in ", round(h1HC0*100, 1), "% cases (HC0) and ", round(h1HC3*100, 1), "% cases (HC3)\n"))
-          cat(paste0("Empirical test size (nominal size 5%): the true H0: beta1=beta2=true values was rejected in ", round(WaldHC0*100, 1), "% cases (HC0) and ", round(WaldHC3*100, 1), "% cases (HC3)\n"))
-          cat(paste0("Empirical test power (nominal size 5%): the false H0: beta1=0 was rejected in ", round(h0HC0*100, 1), "% cases (HC0) and ", round(h0HC3*100, 1), "% cases (HC3)\n"))
-        }
-        cat("\n_______________________________________\n")
-        sink()
+        msg <- paste0((if (heteroskedastic) "Heterosk." else "Homosk."), ", strat. by ", strat.var, ", N=", N, ", ", method, ", MC=", MC, " done in")
+        cat(msg, round(elapsed, 1), "seconds\n")
         
-        print(paste(msg, "in", round(elapsed, 1), "s"))
-        
+        thisrow <- thisrow+1
       }
     }
   }
 }
 
-# Check runlogs.txt in your current working directory to see the numbers!
+write.csv(results, "results.csv", row.names = FALSE)
